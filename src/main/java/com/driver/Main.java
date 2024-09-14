c65474d6a2faa63b8951b18e5979a709c2e3e973
Gaurav Saxena <99750798+Saxena-G@users.noreply.github.com>
2024-09-14 13:29:54 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,7 +14,7 @@ public class Main {
 }
  }
  public static void main(String[] args) {
-   B b = new B();
-   System.out.println(b.meth());
+   B objB = new B();
+   System.out.println(objB.meth());
 }
  }
